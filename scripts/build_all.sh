cdfd185c9fd3bb16db86f06fead13c8fdb20b20c
Mark Gibbs <mark@gibbs.consulting>
2020-10-29 14:47:57 -0700

    Update with second set of packages

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -7,3 +7,12 @@ for i in "pipeline"; do
   docker build -t $i .
   cd ..
 done
+
+#
+# Once built, use docker tag to tag the image
+# and then docker push to upload it
+#
+# eg:
+# docker tag HashGoesHere delsim/pipeline:0.0.0
+# docker push delsim/pipeline
+#
