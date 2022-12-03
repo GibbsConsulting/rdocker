bb9481985003c516b4a36660f9e5b35c6077c2ff
Mark Gibbs <mark@gibbs.consulting>
2022-12-03 09:03:07 -0800

    Add 0.3.1 version of bslib package for bootstrap 5

--- scripts/fetch_packages.sh
+++ scripts/fetch_packages.sh
@@ -5,7 +5,7 @@
 mkdir -p pipeline/local_packages
 pushd pipeline/local_packages
 
-for pack in Rcpp_1.0.6 stringi_1.5.3 shinyBS_0.61 httpuv_1.5.5 RcppTOML_0.1.7 mongolite_2.3.0 sass_0.3.1 magick_2.7.1 farver_2.1.0 MASS_7.3-53; do
+for pack in Rcpp_1.0.6 stringi_1.5.3 shinyBS_0.61 httpuv_1.5.5 RcppTOML_0.1.7 mongolite_2.3.0 sass_0.3.1 magick_2.7.1 farver_2.1.0 MASS_7.3-53 bslib_0.3.1 ; do
     if [ -f $pack.tar.gz ]; then
         echo Skipping $pack
     else
